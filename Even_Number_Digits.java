dd157862fe798ad05e4ae9f02ae692cd5032284e
Asif Ullah <71225756+AsifUllahSazzad@users.noreply.github.com>
2023-05-15 20:49:52 +0600

    Update Even_Number_Digits.java

--- Even_Number_Digits.java
+++ Even_Number_Digits.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Even_Number_Digits {
+public class Even_Number_Digits {
+    public static void main(String[] args)
+    {
+        int[] nums = {12,345,2,6,7896};
+        int ans = findnumber(nums);
+        System.out.println(ans);
+    }
+    public static int findnumber(int[] nums)
+    {
+        int count = 0;
+        for(int i: nums)
+        {
+            if(even(i))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static boolean even(int i)
+    {
+        int number = digits(i);
+        return number%2==0;
+    }
+    public static int digits(int n)
+    {
+        return(int) Math.log10(n)+1;
+    }
 }
