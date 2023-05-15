5c65648d44765e8b004f6dd7abd64afe99da57fa
Asif Ullah <71225756+AsifUllahSazzad@users.noreply.github.com>
2023-05-15 21:56:39 +0600

    Create Contains_Duplicate.java

---
+++ Contains_Duplicate.java
@@ -0,0 +1,24 @@
+public class Contains_Duplicate {
+    public static void main(String[] args)
+    {
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(nums));
+    }
+    public static boolean containsDuplicate(int[] nums)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length;j++)
+            {
+                if(i != j)
+                {
+                    if(nums[i]==nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
