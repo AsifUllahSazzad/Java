35e829f4a30116b740b91a226329ebb98f0c040e
Asif Ullah <71225756+AsifUllahSazzad@users.noreply.github.com>
2023-05-16 22:02:29 +0600

    LeetCode Problems

--- Contains_Duplicate.java
+++ Contains_Duplicate.java
@@ -1,21 +1,21 @@
 public class Contains_Duplicate {
     public static void main(String[] args)
     {
-        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        int[] nums = {237384,237384};
         System.out.println(containsDuplicate(nums));
     }
     public static boolean containsDuplicate(int[] nums)
     {
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=0;j<nums.length;j++)
-            {
-                if(i != j)
-                {
-                    if(nums[i]==nums[j])
-                    {
-                        return true;
+        if(1<=nums.length || nums.length<=Math.pow(10,5)) {
+            for (int i = 0; i < nums.length; i++) {
+                if(-Math.pow(10,9)<=nums[i] || nums[i]<=Math.pow(10,9)){
+                for (int j = 0; j < nums.length; j++) {
+                    if (i != j) {
+                        if (nums[i] == nums[j]) {
+                            return true;
+                        }
                     }
+                }
                 }
             }
         }
