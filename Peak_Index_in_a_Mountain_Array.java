c80491c270e9d4dcb0589b9a609dbae1dbb716a1
Asif Ullah <71225756+AsifUllahSazzad@users.noreply.github.com>
2023-05-22 22:41:04 +0600

    LeetCode Problem

--- Peak_Index_in_a_Mountain_Array.java
+++ Peak_Index_in_a_Mountain_Array.java
@@ -1,17 +1,25 @@
-// https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
 public class Peak_Index_in_a_Mountain_Array{
     public static void main(String[] args)
     {
-        int[] arr = {0,10,5,2};
+        int[] arr = {1,2,3,5,6,4,3,2};
         System.out.println(peakIndexInMountainArray(arr));
     }
     static int peakIndexInMountainArray(int[] arr)
     {
-        int i=0;
-        while(arr[i]<arr[i+1])
+        int start=0;
+        int end = arr.length-1;
+        while(start<end)
         {
-            i++;
+            int mid = start+(end-start)/2;
+            if(arr[mid]<arr[mid+1])
+            {
+                start = mid+1;
+            }
+            else{
+                end=mid;
+            }
         }
-        return i;
+        return end;
     }
 }
